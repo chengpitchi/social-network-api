c440b3b94c4bd8ac6832bc6146d79d1949ae7bf1
Billy Cheng <chengpitchi@yahoo.com>
2023-01-20 18:14:09 +1030

    inital-commit

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create reaction subdocument 
+const reactionSchema = new Schema(
+    {
+        reactionBody: { type: String, 
+                        required: true, 
+                        maxLength: 280}, 
+        username: { type: String, 
+                    required: true }, 
+        createdAt: { type: Date, default: Date.now },        
+    }
+); 
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, 
+                    required: true, 
+                    minLength: 1, 
+                    maxLength: 280},
+    createdAt: { type: Date, default: Date.now },
+    username: { type: String, required: true }, 
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `reactionCount` 
+thoughtSchema.virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  }); 
+
+// Initialize our User model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: { type: String, 
+                required: true, 
+                unique: true, 
+                trim: true 
+            },
+    email: { type: String, 
+                required: true, 
+                unique: true, 
+                // for email validation
+                match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
+            },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],  
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `friendCount`
+userSchema.virtual('friendCount')
+  .get(function () {
+    return this.friends.length;
+  }); 
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
